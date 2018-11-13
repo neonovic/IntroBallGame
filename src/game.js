974855b5591a43fa7c57c32135c1e061a56d2ddc
neonovic <pietr.panak@gmail.com>
2018-11-13 13:43:34 +0100

    Added gamestates, implemented paused gamestate

--- src/game.js
+++ src/game.js
@@ -4,6 +4,13 @@ import Ball from "/src/ball";
 import Brick from "/src/brick";
 import { buildLevel, level1 } from "/src/levels";
 
+const GAMESTATE = {
+  PAUSED: 0,
+  RUNNING: 1,
+  MENU: 2,
+  GAMEOVER: 3
+};
+
 export default class Game {
   constructor(gameWidth, gameHeight) {
     this.gameWidth = gameWidth;
@@ -11,6 +18,7 @@ export default class Game {
   }
 
   start() {
+    this.gamestate = GAMESTATE.RUNNING;
     this.paddle = new Paddle(this);
     this.ball = new Ball(this);
 
@@ -18,10 +26,12 @@ export default class Game {
 
     this.gameObjects = [this.paddle, this.ball, ...bricks];
 
-    new InputHandler(this.paddle);
+    new InputHandler(this.paddle, this);
   }
 
   update(deltaTime) {
+    if (this.gamestate === GAMESTATE.PAUSED) return;
+
     this.gameObjects.forEach(object => object.update(deltaTime));
     this.gameObjects = this.gameObjects.filter(
       object => !object.markedForDeletion
@@ -30,5 +40,24 @@ export default class Game {
 
   draw(ctx) {
     this.gameObjects.forEach(object => object.draw(ctx));
+
+    if (this.gamestate === GAMESTATE.PAUSED) {
+      ctx.rect(0, 0, this.gameWidth, this.gameHeight);
+      ctx.fillStyle = "rgba(0,0,0,0.2)";
+      ctx.fill();
+
+      ctx.font = "30px Arial";
+      ctx.fillStyle = "white";
+      ctx.textAlign = "center";
+      ctx.fillText("Paused", this.gameWidth / 2, this.gameHeight / 2);
+    }
+  }
+
+  togglePause() {
+    if (this.gamestate == GAMESTATE.PAUSED) {
+      this.gamestate = GAMESTATE.RUNNING;
+    } else {
+      this.gamestate = GAMESTATE.PAUSED;
+    }
   }
 }
